f952b80ba9644b8047bdcc821243c332fcad2ab4
xiejunqi <1113267674@qq.com>
2026-09-14 12:23:45 +0800

    init

---
+++ helo1.js
@@ -0,0 +1,5 @@
+"use strict";
+function sumb(a, b) {
+    return a + b;
+}
+console.log(sumb(456, 885));
---
+++ helo1.ts
@@ -0,0 +1,8 @@
+
+function sumb(a: number, b:number)
+{
+    return a + b;
+}
+
+console.log(sumb(456,885));
+
